BlissRoms/platform_packages_apps_Blissify

Pulse: Add bar animation smoothing [3/3]
Change-Id: I31ba1aaf2b28f7eb0c379698003e277852ba2eb2

src/com/blissroms/blissify/fragments/buttons/PulseSettings.java
@@ -70,6 +70,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     ColorPickerPreference mLavaLampColorFrom;
     ColorPickerPreference mLavaLampColorTo;
     SwitchPreference mLavaLampEnabled;
+    SwitchPreference mSmoothingEnabled;
     SystemSettingSeekBarPreference mCustomDimen;
     SystemSettingSeekBarPreference mCustomDiv;
     SystemSettingSeekBarPreference mFilled;
@@ -133,6 +134,11 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         mLavaLampEnabled.setChecked(Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.FLING_PULSE_LAVALAMP_ENABLED, 1, UserHandle.USER_CURRENT) == 1);
         mLavaLampEnabled.setOnPreferenceChangeListener(this);
+
+        mSmoothingEnabled = (SwitchPreference) findPreference("fling_smoothing_enabled");
+        mSmoothingEnabled.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.FLING_PULSE_SMOOTHING_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
+        mSmoothingEnabled.setOnPreferenceChangeListener(this);
 
         int customdimen = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.PULSE_CUSTOM_DIMEN, 14, UserHandle.USER_CURRENT);
@@ -247,6 +253,12 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             boolean enabled = ((Boolean) newValue).booleanValue();
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.FLING_PULSE_LAVALAMP_ENABLED, enabled ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mSmoothingEnabled)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.FLING_PULSE_SMOOTHING_ENABLED, enabled ? 1 : 0,
                     UserHandle.USER_CURRENT);
             return true;
         } else if (preference == mCustomDimen) {

LiquidRemix/android_packages_apps_SmartNavSettings

src/com/android/settings/smartnav/PulseSettings.java
@@ -72,6 +72,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     ColorPickerPreference mLavaLampColorFrom;
     ColorPickerPreference mLavaLampColorTo;
     SwitchPreference mLavaLampEnabled;
+    SwitchPreference mSmoothingEnabled;
     SecureSettingSeekBarPreference mCustomDimen;
     SecureSettingSeekBarPreference mCustomDiv;
     SecureSettingSeekBarPreference mFilled;
@@ -135,6 +136,11 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         mLavaLampEnabled.setChecked(Settings.Secure.getIntForUser(getContentResolver(),
                 Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, 1, UserHandle.USER_CURRENT) == 1);
         mLavaLampEnabled.setOnPreferenceChangeListener(this);
+
+        mSmoothingEnabled = (SwitchPreference) findPreference("fling_smoothing_enabled");
+        mSmoothingEnabled.setChecked(Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.FLING_PULSE_SMOOTHING_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
+        mSmoothingEnabled.setOnPreferenceChangeListener(this);
 
         int customdimen = Settings.Secure.getIntForUser(getContentResolver(),
                 Settings.Secure.PULSE_CUSTOM_DIMEN, 14, UserHandle.USER_CURRENT);
@@ -249,6 +255,12 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             boolean enabled = ((Boolean) newValue).booleanValue();
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, enabled ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mSmoothingEnabled)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_SMOOTHING_ENABLED, enabled ? 1 : 0,
                     UserHandle.USER_CURRENT);
             return true;
         } else if (preference == mCustomDimen) {